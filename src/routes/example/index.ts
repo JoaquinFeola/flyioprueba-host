c8646d69f6a81db5dc5ef10284b9cfaabd7bb5f8
JoaquinFeola <joacoarielfeola@gmail.com>
2026-03-31 22:14:51 -0300

    Add initial wrangler configuration for my-worker

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -4,9 +4,7 @@ const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return 'this is an example'
   })
-  fastify.get('/2', async function (request, reply) {
-    return 'this is an example 2'
-  })
+
 }
 
 export default example
